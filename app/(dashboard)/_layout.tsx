eb943bc62106ac831e2a32c424138560c0b8ebfc
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2025-09-12 18:37:13 +0530

    add profile image and details to the side bar

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -3,9 +3,12 @@ import { Drawer } from "expo-router/drawer";
 import { useNavigation } from "expo-router"
 import { DrawerActions } from "@react-navigation/native";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons"
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FooterTabs from "../../components/footerNav"
 import { DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawer";
+import { auth } from "@/firebase";
+import { User } from "@/types/user";
+import { getUserById } from "@/services/userService";
 
 
 
@@ -13,6 +16,21 @@ const DashboardLayout = () => {
 
   const navigation = useNavigation();
 
+  const [userData, setUserData] = useState<User | null>(null);
+
+  const placeholderURL =  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+
+  useEffect(() => {
+      const existUser = async() =>{
+        if(!auth.currentUser) return;
+
+        const user = await getUserById(auth.currentUser.uid);
+        setUserData(user);
+      };
+
+      existUser();
+
+  }, []);
 
   return (
     <SafeAreaView className="flex-1 bg-white">
@@ -50,12 +68,11 @@ const DashboardLayout = () => {
           <DrawerContentScrollView {...props} className="bg-black flex-1">
             <View className="items-center my-6">
               <Image
-                source={{
-                  uri: "https://via.placeholder.com/150", 
-                }}
+                source={{ uri: userData?.photoURL || placeholderURL }} 
                 className="w-32 h-32 rounded-full border-4 border-gray-300"
               />
-              <Text className="text-white mt-2 font-bold text-lg">Username</Text>
+              <Text className="text-black mt-2 font-bold text-lg">{userData?.name || "Username"}</Text>
+              <Text className="text-gray-400 text-sm">{userData?.email || "Email"}</Text>
             </View>
             <DrawerItemList {...props} />
           </DrawerContentScrollView>
@@ -120,4 +137,4 @@ const DashboardLayout = () => {
   )
 }
 
-export default DashboardLayout
+export default DashboardLayout;
